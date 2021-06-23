16181dd46388c7ad9537c3572a35673531b82bec
Nataly Mora Mancera <tatamora21@gmail.com>
2021-06-23 17:05:37 -0500

    Creados componentes básicos, buscador y resultados

---
+++ src/Components/Buscador.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Buscador = ()=>{
+
+    return (
+        <input placeholder="Ingrese una palabra clave"></input>
+    )
+
+}
+
+export default Buscador;
---
+++ src/Components/Resultados.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Resultado = ()=>{
+
+    return(
+        <div>
+            <h1>Resultados:</h1>
+        </div>
+    )
+}
+
+export default Resultado;
